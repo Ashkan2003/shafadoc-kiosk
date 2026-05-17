ec5c23a40c931c7ee188547a0f5e8b1004bf05fd
ashkan <ashkanpc2003@gmail.com>
2026-05-17 15:38:38 +0330

    setup-some-changes-step-1

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,8 +3,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import App from "./App";
 import AppRouter from "./router";
-import ThemeProvider from "./lib/utils/materialUi";
-import MaterialRTLProvider from "./lib/utils/muiRTL";
+import ThemeProvider from "./lib/materialUi/muiTheme";
+import MaterialRTLProvider from "./lib/materialUi/muiRTL";
 import { StoreProvider } from "./lib/redux/storeProvider";
 
 createRoot(document.getElementById("root")!).render(
